575e3564a9ad0ee3bb233b554b6b194ec67b9224
isacmohn <isacmoh006@gmail.com>
2025-05-18 18:01:05 +0200

    La til ekstra python kode som en del av nye forbedringer

---
+++ Database.py
@@ -0,0 +1,8 @@
+import numpy as numpy
+
+print("Velkommen til DNB")
+
+navn = input("Skriv inn navnet ditt her")
+alder = input("Skriv inn alderen din her")
+
+print("navnet ditt er", navn, "og alderen din er", alder")
