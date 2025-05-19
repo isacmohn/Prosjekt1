c9125548fcaa440f6818dbc1eb13f0b100ec79dc
isacmohn <isacmoh006@gmail.com>
2025-05-19 16:55:41 +0200

    Original av Hei verden

--- Hei_Verden.cs
+++ Hei_Verden.cs
@@ -4,6 +4,8 @@ class Program {
 
     public static void Main() {
         Console.WriteLine("Hei Verden med C#");
+        Console.ReadLine("Tast inn Navnet ditt her");
+        Console.WriteLine("Så lagres dette for deg");
     }
 
 }
